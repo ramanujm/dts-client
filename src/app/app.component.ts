e496359ea77675432c0cf96093d75d06cb9bf29d
ramanujm@yahoo.com <ramanujm@yahoo.com>
2019-12-06 17:14:40 -0500

    --- Auto-Git Commit ---

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-declare var device;
+declare var device: { platform: any; };
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'dts-client';
-
-  ngOnInit() {
+    ngOnInit() {
     // tslint:disable-next-line:only-arrow-functions
     document.addEventListener('deviceready', function() {
     alert(device.platform);
